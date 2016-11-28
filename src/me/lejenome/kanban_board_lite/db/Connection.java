ba5ca699088052c7da32281bc9929e8d124b167b
Moez Bouhlel <bmoez.j@gmail.com>
2016-11-28 16:52:19 +0100

    create idea project

---
+++ src/me/lejenome/kanban_board_lite/db/Connection.java
@@ -0,0 +1,58 @@
+package me.lejenome.kanban_board_lite.db;
+
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Connection {
+    private static java.sql.Connection connection;
+
+    private Connection() {
+    }
+
+    public static void initialize() {
+        if (connection != null)
+            return;
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/distributed", "test", "test");
+        } catch (SQLException e) {
+            throw new Error("Cannot Connect to the Database. Please see the manual for more details");
+        }
+    }
+
+    public static ResultSet executeQuery(String query, Object... args) {
+        ResultSet rs = null;
+        try (PreparedStatement stmt = connection.prepareStatement(query)) {
+            for (int i = 0; i < args.length - 1; i += 2) {
+                stmt.setObject((i / 2) + 1, args[i], (int) args[i + 1]);
+            }
+            rs = stmt.executeQuery();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return rs;
+    }
+
+    public static boolean execute(String query, Object... args) {
+        try (PreparedStatement stmt = connection.prepareStatement(query)) {
+            for (int i = 0; i < args.length - 1; i += 2) {
+                stmt.setObject((i / 2) + 1, args[i], (int) args[i + 1]);
+            }
+            return stmt.execute();
+        } catch (SQLException e) {
+            return false;
+        }
+    }
+
+    public static int executeUpdate(String query, Object... args) {
+        try (PreparedStatement stmt = connection.prepareStatement(query)) {
+            for (int i = 0; i < args.length - 1; i += 2) {
+                stmt.setObject((i / 2) + 1, args[i], (int) args[i + 1]);
+            }
+            return stmt.executeUpdate();
+        } catch (SQLException e) {
+            return -1;
+        }
+    }
+}
